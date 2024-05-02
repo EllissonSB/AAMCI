93436eda886552778fbcbd80ace058db82eb46c7
Ellisson Barbosa <57837838+EllissonSB@users.noreply.github.com>
2024-05-02 10:58:35 -0300

    Update main.c
    ATT uso da hal

--- DHT22/main.c
+++ DHT22/main.c
@@ -2,30 +2,92 @@
 #include "dht22.h"
 
 DHT22_HandleTypeDef hdht22;
-  
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART1_UART_Init(void);
 int main(void) {
   // Inicialize o hardware STM32
-  
+  HAL_Init();
+  SystemClock_Config();
+  MX_GPIO_Init();
+  MX_USART1_UART_Init();
   // Inicialize o sensor DHT22
-  
-  DHT22_Init(&hdht22, GPIOA, 0);
-  
-  // Leitura dos dados do sensor
-  float temperature, humidity;
-  if (DHT22_ReadData(&hdht22, &temperature, &humidity) == DHT22_OK)
-  {
-    // Dados lidos com sucesso
-    // Faça algo com a temperatura e umidade
-    printf("Temperatura: %.2f °C\n", temperature);
-    printf("Umidade: %.2f %%\n", humidity);
-  } 
-  else 
-  {
-    // Erro ao ler os dados do sensor
-    printf("Falha ao ler os dados do sensor DHT22\n");
-  }
-  
+  DHT22_Init(&hdht22, GPIOA, 0); 
   while (1) {
-      // Loop principal do seu programa
+    if (DHT22_ReadData(&hdht22, &temperature, &humidity) == DHT22_OK)
+      {
+        // Dados lidos com sucesso
+        // Faça algo com a temperatura e umidade
+        printf("Temperatura: %.2f °C\n", temperature);
+        printf("Umidade: %.2f %%\n", humidity);
+      } 
+      else 
+      {
+        // Erro ao ler os dados do sensor
+        printf("Falha ao ler os dados do sensor DHT22\n");
+      }
+        Delay_ms(1000);
+      }
+}
+
+void SystemClock_Config(void)//Gerado Pela CUBEIDE
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
   }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+static void MX_USART1_UART_Init(void)
+{
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 1000000;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+}
+static void MX_GPIO_Init(void)
+{
+  __HAL_RCC_GPIOA_CLK_ENABLE();
 }
